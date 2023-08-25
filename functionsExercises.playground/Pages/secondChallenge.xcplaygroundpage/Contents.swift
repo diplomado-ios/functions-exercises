3e5f2aa7ebdc895d1c0bfba88f7c81f554b40cd1
Alejandro Mendoza <zippi180@gmail.com>
2023-08-24 22:26:11 -0600

    Add baseline exercise code

---
+++ functionsExercises.playground/Pages/secondChallenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/// You can ignore line 8, asume that the constant ``groceryList`` will contain an **Array of Strings**
+/// For testing locally you can comment line 8 and remove the `//` from line 9
+
+/// **Remember to comment line 9 and remove the `//` from line 8 before submitting your code**
+
+let groceryList: [String] = Array(CommandLine.arguments.dropFirst())
+//let groceryList: [String] = ["green beans", "milk", "black beans", "pinto beans", "apples"]
+
+
+// Print the array of beans
+
+
+// Print the array of other groceries
